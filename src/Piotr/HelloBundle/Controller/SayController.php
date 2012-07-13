7653845c85975fa7a505e3827126b0e2711609a8
Piotr Suwik <piotr.suwik@kiddivouchers.com>
2012-07-13 16:46:30 +0100

    Playing with Symphony tasks

--- src/Piotr/HelloBundle/Controller/SayController.php
+++ src/Piotr/HelloBundle/Controller/SayController.php
@@ -15,7 +15,53 @@ class SayController extends Controller
 
 	public function sayAction($name)
 	{
-		return $this->render('PiotrHelloBundle:Say:hello.html.twig', array('name' => $name));
+		{# Uses: input.id, input.label, input.class, input.name #}
+			$formfields = array();
+			$formfields[] = array(
+				'id' => 'name',
+				'label' => 'Name',
+				'class' => 'form-name',
+				'name' => 'name'
+			);
+			$formfields[] = array(
+				'id' => 'tel',
+				'label' => 'Telephone no.',
+				'class' => 'form-tel',
+				'name' => 'tel'
+			);
+			
+			$urlList = array(
+				'test!',
+				'something',
+				'Brian',
+				'Soup'
+			);
+
+			return $this->render(
+					'PiotrHelloBundle:Say:hello.html.twig',
+					array(
+					'name' => $name,
+					'url_self' => $this->get('router')->generate('_piotr',
+						array('name' => 'Piotr Suwik')),
+					'form_fields' => $formfields,
+					'urlList' => $urlList,
+					)
+			);
+		}
+	}
+
+	public function getNumbersAction()
+	{
+		$numbers = array();
+
+		for ($i = 0; $i < 10; $i++)
+		{
+			$numbers[] = rand(0, 1000);
+		}
+
+		return $this->render('PiotrHelloBundle::templates/numbersList.html.twig',
+				array('numbers' => $numbers));
 	}
 
 }
+
